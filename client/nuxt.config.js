79ffa1271d4483b5dbd4f30a317cb8e022cb3602
Ninja <ninja@ninja>
2021-12-24 10:15:01 +0100

    opengraph image

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -6,6 +6,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'What\'s this? A minimalist anonymous Q&A website!' },
+      { property: 'og:image', content: '/0w0.svg' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
